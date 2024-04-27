c894c7e469c92145639408bc0c5d1671db4c9cc0
Mushfiq Alam Sami <mushfiq9912@gmail.com>
2024-04-27 17:16:36 +0600

    added delete and update functionality also linked to specified data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,9 +23,67 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const craftCollection = client.db("craftDB").collection("craft");
+
+    // create data
+    app.post("/craft", async (req, res) => {
+      const newCraft = req.body;
+      console.log(newCraft);
+      const result = await craftCollection.insertOne(newCraft);
+      res.send(result);
+    });
+
+    // read data
+    app.get("/craft", async (req, res) => {
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // update data
+    app.get("/craft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put("/craft/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCraft = req.body;
+      const craft = {
+        $set: {
+          itemname: updatedCraft.itemname,
+          subcategory: updatedCraft.subcategory,
+          description: updatedCraft.description,
+          price: updatedCraft.price,
+          rating: updatedCraft.rating,
+          customization: updatedCraft.customization,
+          processtime: updatedCraft.processtime,
+          stockstatus: updatedCraft.stockstatus,
+          useremail: updatedCraft.useremail,
+          username: updatedCraft.username,
+          photo: updatedCraft.photo,
+        },
+      };
+      const result = await craftCollection.updateOne(filter, craft, options);
+      res.send(result);
+    });
+
+    // delete data
+    app.delete("/craft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
